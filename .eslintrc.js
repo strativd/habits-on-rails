5d6fa5a58f7c4f13905a08f34669235958bac416
ztratify <strat.barrett@shopify.com>
2021-04-10 15:21:22 -0700

    Setup eslint javascript

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-console': 'off',
+    'react/prop-types': 0,
+    'prefer-const': 0,
+    'func-names': ['error', 'as-needed'],
+    'no-shadow': 'error',
+  },
+};
